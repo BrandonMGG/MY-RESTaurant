3cde1b5566d188c9c75f5d01eeff88a2fdc5c5ac
traznar <ggbrandonm@gmail.com>
2024-04-06 19:16:53 -0600

    development-frontend_responsive_web

--- frontend/myRestaurant/src/app/app.module.ts
+++ frontend/myRestaurant/src/app/app.module.ts
@@ -6,18 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MenuComponent } from './menu/menu.component';
-
+import { ReservaComponent } from './reserva/reserva.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ReservaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/myRestaurant/src/app/reserva/reserva.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
+@Component({
+  selector: 'app-reserva',
+  templateUrl: './reserva.component.html',
+  styleUrls: ['./reserva.component.scss']
+})
+export class ReservaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  selectedDate: Date;
+  minDate = new Date(); // Today's date as minimum selectable date
+
+  onDateChange(event : Event) {
+   // this.selectedDate = new Date(dateStr);
+  }
+
+  makeReservation() {
+    // Implement logic to handle reservation based on selectedDate
+    console.log("Reservation requested for:", this.selectedDate);
+    // You can make an API call, show a confirmation message etc. here
+  }
+
+
+
+}
