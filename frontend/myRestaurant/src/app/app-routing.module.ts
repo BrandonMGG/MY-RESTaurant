b10dbad985ca35793aa3c682e5da4f02e6738fa2
traznar <ggbrandonm@gmail.com>
2024-05-14 00:04:11 -0600

    Login Integration

--- frontend/myRestaurant/src/app/app-routing.module.ts
+++ frontend/myRestaurant/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReservaAdminComponent } from './reserva-admin/reserva-admin.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
-  { path: 'adminreserva', component: ReservaAdminComponent }
+  { path: 'adminreserva', component: ReservaAdminComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
