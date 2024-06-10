5e35f006834ca2e2a5ebdfa783f90c2592e97b36
traznar <ggbrandonm@gmail.com>
2024-06-09 22:37:02 -0600

    Delete Working

--- microservices/delRes.py
+++ microservices/delRes.py
@@ -11,22 +11,24 @@ def eliminar_reservaciones():
     if request.method == 'OPTIONS':
         return jsonify({'message': 'OK'}), 200
     try:
+        # Log the request content type
+        print(f"Content-Type: {request.content_type}")
+        
         if request.is_json:
             data = request.get_json()
             print(f"Request JSON: {data}")
             # abre el archivo JSON
             with open('reservacionesDB.json') as f:
                 reservaciones = json.load(f)
-            #obtener el ID
+            # obtener el ID
             idRes = data.get('id')
             idRes = str(idRes)
             # recorrer cada fecha buscando el id y borra la reservacion
             for date in reservaciones["reservaciones"]:
-                for res in reservaciones["reservaciones"][date]:
-                    if res == idRes:
-                        # obtener la fecha para liberar en disponibles
-                        del reservaciones["reservaciones"][date][idRes]
-                        break
+                if idRes in reservaciones["reservaciones"][date]:
+                    # eliminar la reservacion
+                    del reservaciones["reservaciones"][date][idRes]
+                    break
             # Escribir los datos modificados de nuevo en el archivo
             with open('reservacionesDB.json', 'w') as archivo:
                 json.dump(reservaciones, archivo, indent=4)
@@ -36,6 +38,5 @@ def eliminar_reservaciones():
     except FileNotFoundError:
         return jsonify({"respuesta":"El archivo de reservaciones no se ha encontrado."}), 404
 
-
 if __name__ == '__main__':
-    app.run(port=6000)
+    app.run(port=5001)
