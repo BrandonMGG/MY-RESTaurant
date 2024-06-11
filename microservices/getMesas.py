e8f7115b556e7fcefd3d9583f46adb0bbdb39750
Mario Carranza <mjcarranza@estudiantec.cr>
2024-06-10 21:22:44 -0600

    Update getMesas.py

--- microservices/getMesas.py
+++ microservices/getMesas.py
@@ -1,25 +1,26 @@
+
 import sys
 from flask import Flask, jsonify
-from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')  # Conexión a la instancia local de MongoDB
-db = client['soaDB']  # Reemplaza 'tu_base_de_datos' por el nombre de tu base de datos
-collection = db['dataBase']  # Reemplaza 'tu_coleccion' por el nombre de tu colección en MongoDB
 
+# Ruta para agregar horas al horario
 @app.route('/getMesas', methods=['GET'])
 def get_mesas():
     try:
-        datos = list(collection.find({}, {'_id': 0}))  # Recuperar todas las reservaciones
-        print(datos[0]["mesas"])
-        response = jsonify({"mesas": datos[0]["mesas"]})
+        # abre el archivo JSON
+        with open('reservacionesDB.json') as f:
+            reservaciones = json.load(f)
+        response = jsonify({"mesas":reservaciones["mesas"]})
         # Agregar encabezados CORS
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
         return response
-    except Exception as e:
-        response = jsonify({"respuesta": f"Error al obtener las reservaciones: {str(e)}"}), 500
+    except FileNotFoundError:
+        
+        response = jsonify({"respuesta":"El archivo de reservaciones no se ha encontrado."}), 404
         # Agregar encabezados CORS
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
